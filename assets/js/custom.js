79772c15e6ce3ddbaa02a49837ac80c3e92373a1
Jahangir Alam <jahangir.vsv@gmail.com>
2021-12-06 13:40:30 +0800

    profile picture problem done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -26,3 +26,7 @@ $(document).ready(function () {
   //     uiLibrary: 'bootstrap4'
   // });
 });
+$(".custom-file-input").on("change", function () {
+  var fileName = $(this).val().split("\\").pop();
+  $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
